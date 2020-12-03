06ffdfe49ab003de8a604f9b4087ddc27f8c5c40
Lucas Sampaio <lucas@sampaio.us>
2020-12-02 23:46:50 -0300

    first draft

---
+++ src/components/Profile.js
@@ -0,0 +1,13 @@
+import React, { useState } from "react"
+import ProfileForm from "./ProfileForm"
+
+const Profile = ({updateInput}) => {
+  return (
+  <div>
+    <i className="fas fa-pen-square"></i>
+    <h3>Lucas Sampaio</h3>
+    <h2>Email</h2>
+  </div>)
+}
+
+export default Profile
