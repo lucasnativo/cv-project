26e3820af7da22edd5c7231435a5408790508151
Lucas Sampaio <lucas@sampaio.us>
2020-12-14 20:15:56 -0300

    done the social component

--- src/components/Social.js
+++ src/components/Social.js
@@ -1,16 +1,58 @@
-import React from "react";
+import React, {useState} from "react";
 
 const Social = () => {
+  const [edit, setEdit] = useState(false)
+  const [social, setSocial] = useState([{site: "Facebook", link: "facebook.com/lucas", id: 0}])
+  const [inputSite, setInputSite] = useState("")
+  const [inputLink, setInputLink] = useState("")
+  const [id, setId] = useState(1)
+
+
+  const onChangeSite = (e) => {
+    setInputSite(e.target.value)
+  }
+
+  const onChangeLink = (e) => {
+    setInputLink(e.target.value)
+  }
+
+  const deleteSite = (id) => {
+    const removedArr = [...social].filter(site => site.id !== id)
+    setSocial(removedArr)
+  }
+
+  const onSubmit = (e) => {
+    e.preventDefault()
+
+    setSocial([...social, {site: inputSite, link: inputLink, id: id}])
+    setInputLink("")
+    setInputSite("")
+    setId(id + 1)
+  }
+
   return (
     <div>
-      <button>Edit</button>
-      <ul>
-        <li>Facebook</li>
-        <li>Instagram</li>
-        <li>Twitter</li>
-      </ul>
-    </div>
-  );
+      <h3>Social</h3>
+      <button onClick={() => setEdit(!edit)}>Edit</button>
+      {edit ? (
+        <div>
+          <form onSubmit={onSubmit}>
+            <label>Site: <input value={inputSite} onChange={onChangeSite} /></label>
+            <label>Link: <input value={inputLink} onChange={onChangeLink}/></label>
+            <button>Add</button>
+          </form>
+          <ul>
+          {social.map(item => (<li>{item.site}: {item.link} <button onClick={() => deleteSite(item.id)}>X</button></li>))}
+          </ul>
+      </div>
+      ) : (
+        <>
+          <ul>
+            {social.map(item => (<li>{item.site}: {item.link}</li>))}
+          </ul>
+        </>
+      )}     
+    </div>  );
 };
 
 export default Social;
